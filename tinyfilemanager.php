8db1aa6fba8badef6f4ef56ef4b5f872b7b6b044
theanh <theanh@ilovex.co.jp>
2019-11-18 15:38:30 +0900

    fix(fm_get_filesize function): fm_get_filesize sometime uses 'Folder' string as input param.

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -2243,6 +2243,9 @@ function fm_get_size($file)
  */
 function fm_get_filesize($size)
 {
+    if (!is_numeric($size)) {
+        return $size; // string: 'Folder'
+    }
     $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
     $power = $size > 0 ? floor(log($size, 1024)) : 0;
     return sprintf('%s %s', round($size / pow(1024, $power), 2), $units[$power]);
